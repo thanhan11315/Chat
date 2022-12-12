ae39dc501a05c85017bc798097618d10b57dac1e
anle <thanhan11315@gmail.com>
2022-12-12 21:51:11 +0700

    box-image 12/12

--- src/components/boxImage/boxImage.jsx
+++ src/components/boxImage/boxImage.jsx
@@ -1,10 +1,12 @@
 import {
   CloseOutlined,
   DownloadOutlined,
+  DownOutlined,
   LayoutOutlined,
   RedoOutlined,
   ShareAltOutlined,
   UndoOutlined,
+  UpOutlined,
   ZoomInOutlined,
   ZoomOutOutlined,
 } from "@ant-design/icons";
@@ -12,11 +14,53 @@ import React from "react";
 import "./boxImage.scss";
 import ImageVideo from "../../assets/images/Video.png";
 import AvatarAnLe from "../../assets/images/AvatarAnLe.jpg";
+import AvatarAnhHoai from "../../assets/images/AvatarAnhHoai.jpg";
+import AvatarCTO from "../../assets/images/AvatarCTO.jpg";
+import SuperShipLogo from "../../assets/images/SuperShipLogo.png";
 
-function BoxImage(props) {
+function BoxImage() {
   const handleClicCloseIframeFile = () => {
     const element = document.querySelector(".box-show-image");
     element.style.display = "none";
+  };
+
+  let slideIndex = 1;
+
+  const showSlides = (n) => {
+    let i;
+    let slides = document.querySelectorAll(".image-show-body .image-show");
+    console.log(slides);
+    console.log("1");
+    // let dots = document.querySelectorAll(".image-show-list .img-sh-th");
+    // console.log(dots);  && dots.length > 1
+    if (slides.length > 1) {
+      if (n > slides.length) {
+        slideIndex = 1;
+      }
+      if (n < 1) {
+        slideIndex = slides.length;
+      }
+      for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+      }
+      // for (i = 0; i < dots.length; i++) {
+      //   dots[i].className = dots[i].className.replace("active", "");
+      // }
+      slides[slideIndex - 1].style.display = "block";
+      // dots[slideIndex - 1].className += "active";
+    }
+  };
+
+  showSlides(slideIndex);
+
+  const plusSlides = (n) => {
+    console.log(1);
+    showSlides((slideIndex += n));
+  };
+
+  const currentSlide = (n) => {
+    console.log(1);
+    showSlides((slideIndex = n));
   };
   return (
     <>
@@ -24,48 +68,108 @@ function BoxImage(props) {
         <div className="box-image-1">
           <div className="image-show-title">
             <div className="box-1">Cloud của tôi</div>
-            <div className="box-2" onClick={handleClicCloseIframeFile}>
+            <div
+              className="box-2"
+              onClick={() => {
+                handleClicCloseIframeFile();
+                console.log(1);
+              }}
+            >
               <CloseOutlined />
             </div>
           </div>
           <div className="box-show-image-content-history">
             <div className="image-show-content-layout">
               <div className="image-show-body">
-                <div className="image-show-btn-left"></div>
+                <div className="box-btn">
+                  <div
+                    className="image-show-btn-left"
+                    onClick={() => {
+                      plusSlides(-1);
+                    }}
+                  >
+                    <DownOutlined />
+                  </div>
+                </div>
                 <div className="image-show">
                   <img src={ImageVideo} alt="not load" />
                 </div>
-                <div className="image-show-btn-right"></div>
+                <div className="image-show">
+                  <img src={AvatarAnhHoai} alt="not load" />
+                </div>
+                <div className="image-show">
+                  <img src={AvatarCTO} alt="not load" />
+                </div>
+                <div className="image-show">
+                  <img src={SuperShipLogo} alt="not load" />
+                </div>
+                <div className="box-btn">
+                  <div
+                    className="image-show-btn-right"
+                    onClick={() => {
+                      plusSlides(1);
+                    }}
+                  >
+                    <UpOutlined />
+                  </div>
+                </div>
               </div>
             </div>
             <div className="image-history">
               <div className="timeline-slider">
                 <div className="pivot-top"></div>
+                <div className="pivot-handle"></div>
                 <div className="line"></div>
                 <div className="pivot-bottom"></div>
               </div>
               <div className="image-show-list">
-                <div className="image-show-thumb-legend">12/6</div>
+                <div
+                  className="image-show-thumb-legend"
+                  onClick={() => {
+                    console.log(1);
+                  }}
+                >
+                  12/6
+                </div>
                 <div className="img-sh-th">
-                  <img src={ImageVideo} alt="not load" />
+                  <img
+                    src={ImageVideo}
+                    alt="not load"
+                    onClick={() => {
+                      console.log(1);
+                      currentSlide(1);
+                    }}
+                  />
                 </div>
                 <div className="image-show-thumb-legend">12/6</div>
                 <div className="img-sh-th">
-                  <img src={ImageVideo} alt="not load" />
+                  <img
+                    src={AvatarAnhHoai}
+                    alt="not load"
+                    onClick={() => currentSlide(2)}
+                  />
                 </div>
                 <div className="image-show-thumb-legend">12/6</div>
                 <div className="img-sh-th">
-                  <img src={ImageVideo} alt="not load" />
+                  <img
+                    src={AvatarCTO}
+                    alt="not load"
+                    onClick={() => currentSlide(3)}
+                  />
                 </div>
                 <div className="image-show-thumb-legend">12/6</div>
                 <div className="img-sh-th">
-                  <img src={ImageVideo} alt="not load" />
+                  <img
+                    src={SuperShipLogo}
+                    alt="not load"
+                    onClick={() => currentSlide(4)}
+                  />
                 </div>
               </div>
             </div>
           </div>
         </div>
-        <div className="box-footer">
+        <div className="box-footer" onClick={console.log(1)}>
           <div className="box-1">
             <div className="image">
               {/* <img src={props.valueFile.avatar} alt="not load" /> */}
@@ -85,7 +189,11 @@ function BoxImage(props) {
             </div>
           </div>
           <div className="box-2">
-            <div>
+            <div
+              onClick={() => {
+                console.log(1);
+              }}
+            >
               <ShareAltOutlined />
             </div>
             <div>
